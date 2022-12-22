5d49ad2298cd67f879ee59705a3ad571210820e0
Aleksandr Kalinin <akalinin@sfedu.ru>
2022-12-22 14:34:36 +0300

    Show Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def show
+    @users = User.find_by_id(params[:id])
+  end
   def destroy
     @users = User.find(params[:id])
     @users.destroy
-    redirect_to users_path, notice: 'User deleted.'
+    redirect_to home_path, notice: 'User deleted.'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
   get 'contacts/index'
   get 'users/index'
+  get 'persons/profile'
+  get 'about' => 'pages#about', as: "about"
   devise_for :users
-
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  get 'persons/profile'
-  match '/users',   to: 'users#index',   via: 'get'
   root 'posts#index', as: "home"
 
-  delete "users/:id", to: "users#destroy"
-
-  get 'about' => 'pages#about', as: "about"
    get 'persons/profile', as: 'user_root'
+  resources :users, only: [:show, :index]
   resources :posts, only: [:show, :index] do
     resources :comments
   end
