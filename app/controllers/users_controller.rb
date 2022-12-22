53f4c91e96ef15ef5851eb64843a98a6fbb04433
Aleksandr Kalinin <akalinin@sfedu.ru>
2022-12-22 22:28:03 +0300

    Refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    @users = User.all
   end
   def show
     @user = User.find_by_id(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,27 @@
 Rails.application.routes.draw do
+
+  root 'posts#index', as: "home"
+  
+  match '/users',   to: 'users#index',   via: 'get'
+  match '/users/:id',     to: 'users#show',       via: 'get'
   get 'contacts/index'
   get 'users/index'
   get 'persons/profile'
   get 'about' => 'pages#about', as: "about"
-  devise_for :users
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-  root 'posts#index', as: "home"
 
+  delete "users/:id", to: "users#destroy"
    get 'persons/profile', as: 'user_root'
   resources :users, only: [:show, :index]
-  delete "users/:id", to: "users#destroy"
+
   resources :posts, only: [:show, :index] do
     resources :comments
   end
   resources :contacts
   namespace :admin do
-
     resources :posts, except: [:show, :index]
+  end
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 end
